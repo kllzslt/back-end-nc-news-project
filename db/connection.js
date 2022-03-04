fee1db019926cb29b03c4cf0c5dcd5c08eb82b8b
Zsolt Kallai <kallaizs@gmail.com>
2022-03-04 10:12:36 +0000

    Modified listen file

--- db/connection.js
+++ db/connection.js
@@ -9,8 +9,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 };
 
-const config = ENV === 'production'
-? {
+const config = ENV === 'production' ? {
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
