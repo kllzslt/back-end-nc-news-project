6f68a4941cb6e7bb12850eb1c86b1c97a5b2f6b4
Zsolt Kallai <kallaizs@gmail.com>
2022-03-06 19:25:22 +0000

    fixed issues with more2 test

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,6 +1,6 @@
 exports.getEndpoints = (_, res, next) => {
 	res.status(200).send({
-		"GET /api": {
+		"GET /": {
 			description:
 				"Responds with a JSON object detailing the available endpoints",
 		},
